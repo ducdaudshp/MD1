3d874d402a61f0bc846854561d4cb264d15c25dd
ducdaudshp <ducdaudshp@gmail.com>
2022-01-05 11:15:40 +0700

    Thực hành 2

---
+++ 5thang1/js1.js
@@ -0,0 +1,6 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt('Nhập chiều dài: ');
+inputHeight = prompt('Nhập chiều rộng: ');
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
