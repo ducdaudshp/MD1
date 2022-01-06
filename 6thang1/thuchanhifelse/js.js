470d59e6154b4af89b9cf2527ee6fe4af718ed19
ducdaudshp <ducdaudshp@gmail.com>
2022-01-06 16:29:08 +0700

    thuc hanh if else

---
+++ 6thang1/thuchanhifelse/js.js
@@ -0,0 +1,7 @@
+function check(){
+    let text1 = document.getElementById('nhap').value;
+    if (text1 == "ECMAScript")
+        alert('Right')
+    else
+        alert('Didn’t know? ECMAScript!')
+}
